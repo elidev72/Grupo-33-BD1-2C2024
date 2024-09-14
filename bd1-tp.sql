dddb4ef778cceb57d708fe6e31f5e09f28bcec0d
EliasRamil <raeli0072@gmail.com>
2024-09-13 22:18:58 -0300

    ABM concecionarias terminado

--- bd1-tp.sql
+++ bd1-tp.sql
@@ -34,6 +34,35 @@ CREATE TABLE `Automotriz` (
 -- Dumping data for table `Automotriz`
 --
 
+/*!40000 ALTER TABLE `Automotriz` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Automovil`
+--
+
+DROP TABLE IF EXISTS `Automovil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Automovil` (
+  `chasis` int NOT NULL AUTO_INCREMENT,
+  `patente` varchar(45) NOT NULL,
+  `fechaInicio` date NOT NULL,
+  `fechaFinalizacion` date DEFAULT NULL,
+  `idLíneaDeMontaje` int NOT NULL,
+  `idPedidoAutos` int NOT NULL,
+  PRIMARY KEY (`chasis`),
+  KEY `fk_Automovil_LíneaDeMontaje1_idx` (`idLíneaDeMontaje`),
+  KEY `fk_Automovil_PedidoAutos1_idx` (`idPedidoAutos`),
+  CONSTRAINT `fk_Automovil_LíneaDeMontaje1` FOREIGN KEY (`idLíneaDeMontaje`) REFERENCES `LíneaDeMontaje` (`idLíneaDeMontaje`),
+  CONSTRAINT `fk_Automovil_PedidoAutos1` FOREIGN KEY (`idPedidoAutos`) REFERENCES `PedidoAutos` (`idPedidoAutos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Automovil`
+--
+
 LOCK TABLES `Automovil` WRITE;
 /*!40000 ALTER TABLE `Automovil` DISABLE KEYS */;
 /*!40000 ALTER TABLE `Automovil` ENABLE KEYS */;
@@ -51,16 +80,13 @@ CREATE TABLE `Concesionaria` (
   `nombre` varchar(45) NOT NULL,
   `direccion` varchar(45) NOT NULL,
   PRIMARY KEY (`idConcesionaria`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `Concesionaria`
 --
 
-LOCK TABLES `Concesionaria` WRITE;
-/*!40000 ALTER TABLE `Concesionaria` DISABLE KEYS */;
-INSERT INTO `Concesionaria` VALUES (1,'Concesionaria Prueba','Calle de la Prueba 123');
 /*!40000 ALTER TABLE `Concesionaria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,6 +110,31 @@ CREATE TABLE `EstaciónDeTrabajo` (
 
 --
 -- Dumping data for table `EstaciónDeTrabajo`
+--
+
+/*!40000 ALTER TABLE `EstaciónDeTrabajo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `InformePedido`
+--
+
+DROP TABLE IF EXISTS `InformePedido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `InformePedido` (
+  `idInformePedido` int NOT NULL AUTO_INCREMENT,
+  `montoTotal` double NOT NULL,
+  `fechaEntregaEstimada` date NOT NULL,
+  `idPedidoAutos` int NOT NULL,
+  PRIMARY KEY (`idInformePedido`),
+  KEY `fk_InformePedido_PedidoAutos1_idx` (`idPedidoAutos`),
+  CONSTRAINT `fk_InformePedido_PedidoAutos1` FOREIGN KEY (`idPedidoAutos`) REFERENCES `PedidoAutos` (`idPedidoAutos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `InformePedido`
 --
 
 LOCK TABLES `InformePedido` WRITE;
@@ -165,8 +216,35 @@ CREATE TABLE `InsumosRecibidos` (
 -- Dumping data for table `InsumosRecibidos`
 --
 
-LOCK TABLES `ListaDeInsumosPedidos` WRITE;
-/*!40000 ALTER TABLE `ListaDeInsumosPedidos` DISABLE KEYS */;
+LOCK TABLES `InsumosRecibidos` WRITE;
+/*!40000 ALTER TABLE `InsumosRecibidos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `InsumosRecibidos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ListaDeInsumosPedidos`
+--
+
+DROP TABLE IF EXISTS `ListaDeInsumosPedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ListaDeInsumosPedidos` (
+  `idlLstaDeInsumosPedidos` int NOT NULL AUTO_INCREMENT,
+  `fecha` date NOT NULL,
+  `idLíneaDeMontaje` int NOT NULL,
+  `idProveedor` int NOT NULL,
+  PRIMARY KEY (`idlLstaDeInsumosPedidos`),
+  KEY `fk_listaDeInsumosPedidos_LíneaDeMontaje1_idx` (`idLíneaDeMontaje`),
+  KEY `fk_listaDeInsumosPedidos_Proveedor1_idx` (`idProveedor`),
+  CONSTRAINT `fk_listaDeInsumosPedidos_LíneaDeMontaje1` FOREIGN KEY (`idLíneaDeMontaje`) REFERENCES `LíneaDeMontaje` (`idLíneaDeMontaje`),
+  CONSTRAINT `fk_listaDeInsumosPedidos_Proveedor1` FOREIGN KEY (`idProveedor`) REFERENCES `Proveedor` (`idProveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ListaDeInsumosPedidos`
+--
+
 /*!40000 ALTER TABLE `ListaDeInsumosPedidos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -195,28 +273,9 @@ CREATE TABLE `LíneaDeMontaje` (
 -- Dumping data for table `LíneaDeMontaje`
 --
 
-/*!40000 ALTER TABLE `LíneaDeMontaje` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Marca`
---
-
-DROP TABLE IF EXISTS `Marca`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Marca` (
-  `idMarca` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(45) NOT NULL,
-  `info` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`idMarca`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Marca`
---
-
+LOCK TABLES `LíneaDeMontaje` WRITE;
+/*!40000 ALTER TABLE `LíneaDeMontaje` DISABLE KEYS */;
+INSERT INTO `LíneaDeMontaje` VALUES (1,'Línea de Montaje Renault Captur',100,1,1),(2,'Línea de Montaje Volkswagen Gol Trend',120,2,1),(3,'Línea de Montaje Chevrolet Onix',80,3,1);
 LOCK TABLES `Marca` WRITE;
 /*!40000 ALTER TABLE `Marca` DISABLE KEYS */;
 INSERT INTO `Marca` VALUES (1,'Renault Argentina','Marca francesa de automóviles con presencia en Argentina desde hace décadas, conocida por su producción local en Córdoba y su amplia gama de modelos populares en el mercado argentino.'),(2,'Volkswagen Argentina','Marca alemana reconocida por su larga trayectoria en el mercado argentino, con plantas de producción en Pacheco y Córdoba, ofreciendo una amplia gama de vehículos para distintos segmentos.'),(3,'Chevrolet Argentina','Marca estadounidense con presencia histórica en Argentina, destacada por sus modelos icónicos y una variedad de vehículos populares en el país.');
@@ -245,8 +304,6 @@ CREATE TABLE `ModeloDeVehículo` (
 -- Dumping data for table `ModeloDeVehículo`
 --
 
-LOCK TABLES `ModeloPedido` WRITE;
-/*!40000 ALTER TABLE `ModeloPedido` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ModeloPedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -272,6 +329,29 @@ CREATE TABLE `PedidoAutos` (
 
 --
 -- Dumping data for table `PedidoAutos`
+--
+
+LOCK TABLES `PedidoAutos` WRITE;
+/*!40000 ALTER TABLE `PedidoAutos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PedidoAutos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Proveedor`
+--
+
+DROP TABLE IF EXISTS `Proveedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Proveedor` (
+  `idProveedor` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`idProveedor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Proveedor`
 --
 
 LOCK TABLES `Proveedor` WRITE;
@@ -309,7 +389,7 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'bd1-tp'
 --
-/*!50003 DROP PROCEDURE IF EXISTS `ABM_Concesionaria` */;
+/*!50003 DROP PROCEDURE IF EXISTS `Alta_Concesionaria` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
 /*!50003 SET @saved_col_connection = @@collation_connection */ ;
@@ -319,56 +399,32 @@ UNLOCK TABLES;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=`elidev`@`localhost` PROCEDURE `ABM_Concesionaria`(
-	IN accion CHAR(1), 
-    IN id INT,
-    IN nombre_concesionaria VARCHAR(45),
+CREATE DEFINER=`elidev`@`localhost` PROCEDURE `Alta_Concesionaria`(
+	IN nombre_concesionaria VARCHAR(45),
     IN direccion_concesionaria VARCHAR(45),
     OUT nResultado INT,
     OUT cMensaje VARCHAR(255)
 )
 BEGIN
-	DECLARE existe INT;
+    DECLARE existe_nombre INT;
+    DECLARE existe_direccion INT;
 
-    IF accion = 'A' THEN
-        -- Verificar si ya existe una concesionaria con el mismo nombre
-        SELECT COUNT(*) INTO existe FROM Concesionaria WHERE nombre = nombre_concesionaria;
-        IF existe > 0 THEN
-            SET nResultado = -1;
-            SET cMensaje = 'Ya existe una concesionaria con ese nombre.';
+    -- Verificar si ya existe una concesionaria con el mismo nombre
+    SELECT COUNT(*) INTO existe_nombre FROM Concesionaria WHERE nombre = nombre_concesionaria;
+    IF existe_nombre > 0 THEN
+        SET nResultado = -1;
+        SET cMensaje = 'Ya existe una concesionaria con ese nombre.';
+    ELSE
+        -- Verificar si ya existe una concesionaria con la misma dirección
+        SELECT COUNT(*) INTO existe_direccion FROM Concesionaria WHERE direccion = direccion_concesionaria;
+        IF existe_direccion > 0 THEN
+            SET nResultado = -2;
+            SET cMensaje = 'Ya existe una concesionaria con esa dirección.';
         ELSE
             INSERT INTO Concesionaria (nombre, direccion) VALUES (nombre_concesionaria, direccion_concesionaria);
             SET nResultado = 0;
             SET cMensaje = '';
         END IF;
-    
-    ELSEIF accion = 'M' THEN
-        -- Verificar si la concesionaria a modificar existe
-        SELECT COUNT(*) INTO existe FROM Concesionaria WHERE idConcesionaria = id;
-        IF existe = 0 THEN
-            SET nResultado = -2;
-            SET cMensaje = 'La concesionaria a modificar no existe.';
-        ELSE
-            UPDATE Concesionaria SET nombre = nombre_concesionaria, direccion = direccion_concesionaria WHERE idConcesionaria = id;
-            SET nResultado = 0;
-            SET cMensaje = '';
-        END IF;
-    
-    ELSEIF accion = 'B' THEN
-        -- Verificar si la concesionaria a eliminar existe
-        SELECT COUNT(*) INTO existe FROM Concesionaria WHERE idConcesionaria = id;
-        IF existe = 0 THEN
-            SET nResultado = -3;
-            SET cMensaje = 'La concesionaria a eliminar no existe.';
-        ELSE
-            DELETE FROM Concesionaria WHERE idConcesionaria = id;
-            SET nResultado = 0;
-            SET cMensaje = '';
-        END IF;
-
-    ELSE
-        SET nResultado = -4;
-        SET cMensaje = 'Acción no válida. Las opciones son A (Alta), M (Modificación) o B (Baja).';
     END IF;
 END ;;
 DELIMITER ;
@@ -376,7 +432,48 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!50003 DROP PROCEDURE IF EXISTS `Baja_Concesionaria` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`elidev`@`localhost` PROCEDURE `Baja_Concesionaria`(
+	IN id INT,
+    OUT nResultado INT,
+    OUT cMensaje VARCHAR(255)
+)
+BEGIN
+    DECLARE existe INT;
+
+    -- Verificar si la concesionaria a eliminar existe
+    SELECT COUNT(*) INTO existe FROM Concesionaria WHERE idConcesionaria = id;
+    IF existe = 0 THEN
+        SET nResultado = -5;
+        SET cMensaje = 'La concesionaria a eliminar no existe.';
+    ELSE
+        DELETE FROM Concesionaria WHERE idConcesionaria = id;
+        SET nResultado = 0;
+        SET cMensaje = '';
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `Modificacion_Concesionaria` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -386,4 +483,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-13 21:20:59
+-- Dump completed on 2024-09-13 22:17:24
